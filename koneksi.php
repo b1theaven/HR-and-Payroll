41971b7770dbd20596abdf66bac6e9372f545c28
b1theaven <mohammadrizky881@gmail.com>
2026-09-04 08:51:39 +0700

    docs: add initial readme

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// nama host, Username, password dan nama database
-$koneksi = mysqli_connect("", "", "", "" , "");
-// Periksa Koneksi
-if (mysqli_connect_errno()) {
-    echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-error_reporting(0);
-ini_set('display_errors', 0);
